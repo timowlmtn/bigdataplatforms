ea7c654cc16fd9dc40c6752f4f786dffe7a62327
BlackMetalOwl <timburnsowlmtn@gmail.comj>
2021-10-01 09:17:28 -0400

    Set the PLAYLIST_ID primary key on KACT_KEXP_PLAYLIST

--- src/kexp/ddl/create_warehouse_kexp_playlist.sql
+++ src/kexp/ddl/create_warehouse_kexp_playlist.sql
@@ -1,15 +1,16 @@
 create or replace table warehouse.fact_kexp_playlist
 (
-    load_id INT           NOT NULL,
-    play_type string      NOT NULL,
-    airdate TIMESTAMP_LTZ not null,
-    album   STRING        null,
-    artist  STRING        null,
-    song    STRING        null,
-    DW_CREATE_DATE TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP(),
-    DW_CREATE_USER VARCHAR NOT NULL DEFAULT CURRENT_USER(),
-    DW_UPDATE_DATE TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP(),
-    DW_UPDATE_USER VARCHAR NOT NULL DEFAULT CURRENT_USER(),
+    playlist_id    INT primary key identity (1,1),
+    load_id        INT           NOT NULL,
+    play_type      string        NOT NULL,
+    airdate        TIMESTAMP_LTZ not null,
+    album          STRING        null,
+    artist         STRING        null,
+    song           STRING        null,
+    DW_CREATE_DATE TIMESTAMPTZ            DEFAULT CURRENT_TIMESTAMP(),
+    DW_CREATE_USER VARCHAR       NOT NULL DEFAULT CURRENT_USER(),
+    DW_UPDATE_DATE TIMESTAMPTZ            DEFAULT CURRENT_TIMESTAMP(),
+    DW_UPDATE_USER VARCHAR       NOT NULL DEFAULT CURRENT_USER(),
     FOREIGN KEY (load_id) REFERENCES STAGE.raw_kexp_playlist (LOAD_ID)
 );
 
