acc721311c0e92962cf84bc7276eef409158997b
BlackMetalOwl <timburnsowlmtn@gmail.comj>
2022-09-24 08:16:17 -0400

    Add in a calendar dim

---
+++ src/calendar/calendar_dim.sql
@@ -0,0 +1,21 @@
+create or replace table WAREHOUSE.DIM_CALENDAR as
+with years as (
+    select dateadd(day, seq4(), '2010-01-01')::date date
+    from table (generator(rowcount => (14 * 365+ 3)))
+)
+select date
+     , date_part(year, date)       year
+     , date_part(month, date)      month
+     , monthname(date)             month_name
+     , date_part(dayofmonth, date) day_of_mon
+     , date_part(dayofweek, date)  day_of_week
+     , date_part(weekofyear, date) week_of_year
+     , date_part(dayofyear, date)  day_of_year
+     , date_part(weekday, date)    weekday
+
+     , dayname(date)               dayname
+from years;
+
+
+select min(date), max(date)
+from WAREHOUSE.DIM_CALENDAR;
