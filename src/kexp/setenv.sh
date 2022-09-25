c179e81b21bc8cb81fe66d88e517b237e4017d9b
BlackMetalOwl <timburnsowlmtn@gmail.comj>
2022-09-25 11:21:00 -0400

    Refactor the main pipeline code out of Lambda

--- src/kexp/setenv.sh
+++ src/kexp/setenv.sh
@@ -3,4 +3,6 @@ export PYTHONPATH=aws_layers
 # Here is where I set my private S3 Bucket and stage locations
 source ../../../setenv.sh
 export ExportBucket=$EXPORT_BUCKET
+
+# Here I set the root location to for all KEXP Stages in Snowflake
 export ExportStage=stage/kexp
