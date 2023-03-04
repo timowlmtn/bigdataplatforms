4c2670b8e62d865ebe152bc2b8b058751a5f03a1
timowlmtn <timburnsowlmtn@gmail.com>
2023-03-04 12:58:21 -0500

    Read article https://medium.com/@nhandaloussi/using-the-snowflake-terraform-provider-to-create-storage-integrations-b035702ceff6

--- storage_integration/setenv.sh
+++ storage_integration/setenv.sh
@@ -6,6 +6,13 @@ export S3_DEPLOYMENT_BUCKET=${S3_DEPLOYMENT_BUCKET}
 export SNOWFLAKE_BUCKET=customer-acme-data-lake
 export SNOWFLAKE_ROLE_NAME=role-${SNOWFLAKE_BUCKET}
 
+
+
+export SNOWFLAKE_USER="tf-snow"
+export SNOWFLAKE_PRIVATE_KEY_PATH="~/.snowsql/snowflake_tf_snow_key.p8"
+
+export SNOWFLAKE_REGION="AWS_EU_WEST_1"
+
 export SNOWFLAKE_INTEGRATION_ROLE=${SNOWFLAKE_INTEGRATION_ROLE}
 
 export CONNECTION=${DATABASE}
@@ -27,3 +34,8 @@ export STORAGE_AWS_EXTERNAL_ID=MyAccount_SFCRole=StorageIntegrationCode=
 export S3_STORAGE_INTEGRATION=storage_integration_acme_customer
 
 export DATABASE_ADMIN_ROLE=${DATABASE_ADMIN_ROLE}
+
+export TF_PUBLIC_KEY=$(awk 'NR>1{a[++k]=$0}END{for(i=1;i<k;i++)printf "%s",a[i]}' snowflake_tf_snow_key.pub)
+
+export TF_PASSWORD=$(awk 'NR>1{a[++k]=$0}END{for(i=1;i<k;i++)printf "%s",a[i]}' ~/.snowsql/snowflake_tf_snow_key.p8)
+export TF_USER=tf-snow
