62028bda4564e73dca45dd3420b3593b158b7833
timowlmtn <timburnsowlmtn@gmail.com>
2023-03-24 06:15:17 -0400

    Write KEXP to bronze

--- src/kexp/aws_layers/spark_handler/core.py
+++ src/kexp/aws_layers/spark_handler/core.py
@@ -15,11 +15,52 @@
 # Status: Demo Code
 # ------------------------------------------------
 
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, SparkSession
+
+from os import listdir
+from os.path import join
+import fnmatch
+import shutil
+
 class SparkCore:
-    raw_data_folder = None
+    app_name = None
+    bronze_location = None
+    silver_location = None
+    gold_location = None
 
-    def __init__(self, raw_data_folder):
-        self.raw_data_folder = raw_data_folder
+    spark_context = None
+    sql_context = None
 
-    def process_bronze(self):
-        return {"raw_data_folder": self.raw_data_folder}
+    def __init__(self, app_name, root_location):
+        self.app_name = app_name
+        self.bronze_location = f"{root_location}/bronze"
+        self.silver_location = f"{root_location}/silver"
+        self.gold_location = f"{root_location}/gold"
+
+        self.spark_context = SparkContext(appName=f"{app_name}")
+        self.sql_context = SQLContext(self.spark_context)
+
+    def process_raw_to_bronze(self, raw_data_folder, file_match):
+        result = {"raw": [], "bronze": []}
+
+        spark = SparkSession.builder.getOrCreate()
+
+        for file in fnmatch.filter(listdir(raw_data_folder), file_match):
+            result["raw"].append(join(raw_data_folder, file))
+
+            (file_name, file_type) = file.split(".")
+
+            df = spark.read.load(join(raw_data_folder, file),
+                                              format=file_type, inferSchema="true", header="true")
+
+            try:
+                shutil.rmtree(join(self.bronze_location, file_name))
+            except OSError as e:
+                print("Warning: %s : %s" % (join(self.bronze_location, file_name), e.strerror))
+
+            df.write.format("parquet").save(join(self.bronze_location, file_name))
+
+            result["bronze"].append(join(self.bronze_location, file_name))
+
+        return result
--- src/kexp/spark/python/kexp_spark_local.py
+++ src/kexp/spark/python/kexp_spark_local.py
@@ -13,8 +13,10 @@ def main():
         print("run: export RAW_DATA_FOLDER=<your data>")
     else:
         print(f'Running with {os.getenv("RAW_DATA_FOLDER")}')
-        handler = spark_handler.SparkCore(os.getenv("RAW_DATA_FOLDER"))
-        print(json.dumps(handler.process_bronze(), indent=2))
+        handler = spark_handler.SparkCore(app_name="kexp", root_location=".")
+        print(json.dumps(handler.process_raw_to_bronze(
+                raw_data_folder= os.getenv("RAW_DATA_FOLDER"),
+                file_match="import_kexp_playlist.csv"), indent=2))
 
 
 if __name__ == "__main__":
