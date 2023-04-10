bead84b39c179a6ec4691f05b68e4a17b386decb
timowlmtn <timburnsowlmtn@gmail.com>
2023-04-10 07:30:32 -0400

    Complete API to Bronze for all KEXP feeds

--- src/kexp/pyspark/kexp_transform_raw_to_bronze.py
+++ src/kexp/pyspark/kexp_transform_raw_to_bronze.py
@@ -12,10 +12,17 @@ def main():
     if os.getenv("RAW_DATA_FOLDER") is None or os.getenv("DELTA_LAKE_FOLDER") is None:
         exit("run: export RAW_DATA_FOLDER=<your data> and DELTA_LAKE_FOLDER=<your lake location>")
 
+    result = {
+        "sources": {
+            "lake_location": f'{os.getenv("DELTA_LAKE_FOLDER")}/kexp',
+            "raw_location": f'{os.getenv("RAW_DATA_FOLDER")}/kexp'
+        }
+    }
+
     print(f'Running with {os.getenv("RAW_DATA_FOLDER")}')
     catalog = spark_catalog.SparkCatalog(app_name="kexp",
-                                         lake_location=f'{os.getenv("DELTA_LAKE_FOLDER")}/kexp',
-                                         raw_location=f'{os.getenv("RAW_DATA_FOLDER")}/kexp')
+                                         lake_location=result["sources"]["lake_location"],
+                                         raw_location=result["sources"]["raw_location"])
 
     kexp_transform = {
         "hosts": {"table_name": "KEXP_HOST", "change_column_id": "ID"},
@@ -26,10 +33,11 @@ def main():
     }
 
     for raw_folder in kexp_transform.keys():
-        print(json.dumps(catalog.append_bronze(raw_file_match=f"{raw_folder}/**/*.jsonl",
-                                               table_name=kexp_transform[raw_folder]["table_name"],
-                                               change_column_id=kexp_transform[raw_folder]["change_column_id"]),
-                         indent=2))
+        result[raw_folder] = catalog.append_bronze(raw_file_match=f"{raw_folder}/**/*.jsonl",
+                                                   table_name=kexp_transform[raw_folder]["table_name"],
+                                                   change_column_id=kexp_transform[raw_folder]["change_column_id"])
+
+    print(json.dumps(result, indent=2))
 
 
 if __name__ == "__main__":
