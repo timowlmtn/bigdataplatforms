121feefc71efe96f6015c75127deb60686137aff
Tim <timburnsowlmtn@gmail.com>
2023-04-16 15:32:08 -0400

    algorithm practice

--- src/challenges/Readme.md
+++ src/challenges/Readme.md
@@ -1,2 +1,4 @@
 # Coding Challenges
 See https://medium.com/gitconnected/how-i-got-in-to-amazon-microsoft-google-all-from-studying-these-resources-31724508ce0e
+
+
--- src/challenges/test_string_problems.py
+++ src/challenges/test_string_problems.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class SparkCatalogTest(unittest.TestCase):
     """
     String test class: See
@@ -20,3 +21,31 @@ class SparkCatalogTest(unittest.TestCase):
 
         self.assertEqual("odif yltsaeb", "".join(test_string))
 
+    def string_is_in(self, test_string):
+        char_sequence = set()
+        start_idx = 0
+        result = 0
+        for current_idx in range(len(test_string)):
+            print(f"{test_string[current_idx]} {char_sequence}")
+            while test_string[current_idx] in char_sequence:
+                print(f"\t--{char_sequence}")
+                char_sequence.remove(test_string[start_idx])
+                print(f"\t----{char_sequence}")
+                start_idx += 1
+            char_sequence.add(test_string[current_idx])
+            result = max(result, current_idx - start_idx + 1)
+
+        return result
+
+    def test_longest_string(self):
+        """
+        Given a string s, find the length of the longest substring
+             without repeating characters.
+
+
+        :return:
+        """
+        test_string = "Foot Lights and Spot Lights by Otis Skinner"
+
+        print("\n" +test_string)
+        print(self.string_is_in(test_string))
