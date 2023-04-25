0f44b62ca26306f396f9c236ba7e7d10d072d69c
Tim <timburnsowlmtn@gmail.com>
2023-04-25 13:00:40 -0400

    Create environment database catalog

--- src/terraform/layers/snowflake/database.tf
+++ src/terraform/layers/snowflake/database.tf
@@ -3,3 +3,12 @@ resource "snowflake_database" "catalog" {
   comment                     = "The Database Catalog for Building Data Products using Value Gap Matrix"
   data_retention_time_in_days = 14
 }
+
+resource "snowflake_schema" "landing_zone" {
+  database = upper("${var.environment}_CATALOG")
+  name     = "LANDING_ZONE"
+  comment  = "The Landing zone schema for new data"
+
+  is_transient        = false
+  is_managed          = false
+}
