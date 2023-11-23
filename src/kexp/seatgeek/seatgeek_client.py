3bb5c3542c2ba573d824cbad2b4c1ebd79560159
Tim <timburnsowlmtn@gmail.com>
2023-11-23 13:31:26 -0500

    Seatgeek client

--- src/kexp/seatgeek/seatgeek_client.py
+++ src/kexp/seatgeek/seatgeek_client.py
@@ -7,20 +7,32 @@ import subprocess
 from requests.auth import HTTPBasicAuth
 
 
-def get_resource(sg_client, resource, latitude, longitude, geoip):
-    url = f"https://api.seatgeek.com/2/{resource}"
+def get_resource(sg_client, resource, geoip, args):
+    url = f"https://api.seatgeek.com/2/{resource}?"
 
-    if latitude and longitude:
-        url += f"?lat={latitude}&lon={longitude}"
+    if args.latitude and args.longitude:
+        url += f"lat={args.latitude}&lon={args.longitude}"
     elif geoip:
-        url += f"?geoip={geoip}"
+        url += f"geoip={geoip}"
+        if args.range:
+            url += f"&range={args.range}"
 
-    result = requests.get(url, auth=sg_client)
-    if result.status_code == 200:
-        result = json.dumps(json.loads(result.text), indent=2)
+    if args.type:
+        url += f"&type={args.type}"
+
+    if args.performers:
+        url += f"&q={args.performers}"
+
+    if args.debug:
+        result = f"URL: {url}"
     else:
-        print(url)
-        print(f"ERROR {result.status_code}: {result.text}")
+
+        result = requests.get(url, auth=sg_client)
+        if result.status_code == 200:
+            result = json.dumps(json.loads(result.text), indent=2)
+        else:
+            print(url)
+            print(f"ERROR {result.status_code}: {result.text}")
 
     return result
 
@@ -36,7 +48,14 @@ def main():
     parser.add_argument("--latitude", help="An optional latitude")
     parser.add_argument("--longitude", help="An optional longitude")
 
+    parser.add_argument("--debug", action='store_true', help="Do not run the query, just print the URL")
+
     parser.add_argument("--geoip", action='store_true', help="The current IP to be geolocated")
+    parser.add_argument("--range", help="A range to include if specified")
+
+    parser.add_argument("--type", help="Type of event: concert or theatre for example")
+
+    parser.add_argument("--performers", help="The performers of the event")
 
     args = parser.parse_args()
 
@@ -53,9 +72,8 @@ def main():
 
         result = get_resource(HTTPBasicAuth(os.getenv("seatgeek_client_id"), os.getenv("seatgeek_secret")),
                               resource=args.get,
-                              latitude=args.latitude,
-                              longitude=args.longitude,
-                              geoip=ip_add)
+                              geoip=ip_add,
+                              args=args)
 
     print(result)
 
